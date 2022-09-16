68389c4daa9057bb970cc740b0a4de8eede99d32
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-16 13:53:58 +0200

    INACABADO

--- act6.js
+++ act6.js
@@ -1,9 +1,11 @@
 var cadena = prompt("Escribe una cadena");
 var contador = 0;
-document.write("La longitud de la cadena es " + cadena.length + "caracteres");
-document.write("La cadena en mayusculas es: " + cadena.toUpperCase);
-document.write("La cadena en minusculas es: " + cadena.toLowerCase);
-document.write("CADENA NORMAL");
-document.write(cadena.split(" "));
-document.write(cadena.reverse);
+document.write("La longitud de la cadena es " + cadena.length + " caracteres <br>");
+document.write("La cadena en mayusculas es: " + cadena.toUpperCase + "<br>");
+document.write("La cadena en minusculas es: " + cadena.toLowerCase + "<br>");
+document.write("CADENA NORMAL" + "<br>");
+var palabras = cadena.split(" ");
+document.write(palabras);
+document.write("<br>");
+
 
