39bec1a34e302771356fd7a1e6ba2508375987e0
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-19 10:21:47 +0200

    casi acabado

--- act6.js
+++ act6.js
@@ -1,11 +1,11 @@
 var cadena = prompt("Escribe una cadena");
 var contador = 0;
 document.write("La longitud de la cadena es " + cadena.length + " caracteres <br>");
-document.write("La cadena en mayusculas es: " + cadena.toUpperCase + "<br>");
-document.write("La cadena en minusculas es: " + cadena.toLowerCase + "<br>");
+document.write("La cadena en mayusculas es: " + cadena.toUpperCase() + "<br>");
+document.write("La cadena en minusculas es: " + cadena.toLowerCase() + "<br>");
 document.write("CADENA NORMAL" + "<br>");
 var palabras = cadena.split(" ");
-document.write(palabras);
-document.write("<br>");
-
+document.write(cadena + "<br>");
+document.write("CADENA AL REVES" + "<br>");
+document.write(palabras.reverse() + "<br>");
 
