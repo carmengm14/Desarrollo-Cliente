30145c8ce97b585ea55d67e63f6f86ae35284003
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-19 12:05:53 +0200

    .

--- act2/act2.js
+++ act2/act2.js
@@ -1,17 +1,25 @@
 function mes(mes){
     switch (mes) {
-        case "marzo", "abril", "mayo", "junio":
-            alert(mes + " ES DE: PRIMAVERA");
-            break;
-        case "julio", "agosto", "septiembre":
-            alert(mes + " ES DE: VERANO");
-            break;
-        case "octubre", "noviembre", "diciembre":
-            alert(mes +" ES DE: OTOÑO");
-            break;
-        case "enero", "febrero":
-            alert(mes +" ES DE: INVIERNO");
-            break;
+        case "marzo":
+            case "abril":
+                case "mayo":
+                    case "junio":
+                        alert(mes + " ES DE: PRIMAVERA");
+                        break;
+        case "julio":
+            case "agosto":
+                case "septiembre":
+                    alert(mes + " ES DE: VERANO");
+                    break;
+        case "octubre":
+            case "noviembre":
+                case "diciembre":
+                    alert(mes +" ES DE: OTOÑO");
+                    break;
+        case "enero":
+            case "febrero":
+                alert(mes +" ES DE: INVIERNO");
+                break;
         default:
             alert(mes +" NO ES UN MES VALIDO");
             break;
