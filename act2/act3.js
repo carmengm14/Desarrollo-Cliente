e3bdb9e3f9364fce2543bf9f5c379fe3ea31063d
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-20 10:02:30 +0200

    hecho

--- act2/act3.js
+++ act2/act3.js
@@ -1,2 +1,6 @@
 document.write("<h1>"+"Bienvenido a mi página"+ "</h1>")
-document.write("Tu Navegador es: "+ navigator.appName + "<br>");
+document.write("Tu Navegador es: "+ navigator.appCodeName + "<br>");
+var confirmacion = window.confirm("CONTINUAR");
+if (confirmacion == true) {
+    document.write("CONTINUAMOS");
+}
