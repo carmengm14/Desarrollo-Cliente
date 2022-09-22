a2017e6cbec63aa174a306878d6a3abdfbd3fd23
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-22 13:42:22 +0200

    .

--- act2/act7.js
+++ act2/act7.js
@@ -1,11 +1,3 @@
-function cambiarColorVerde() {
-    document.bgColor = "#71c55b";
-}
-
-function cambiarColorAzul() {
-    document.bgColor = "#0404e2";
-}
-
-function cambiarColorRojo() {
-    document.bgColor = "#e00000";
+function cambiarColor($color) {
+    document.bgColor = $color;
 }
