e37fee66f6a109f67940c2dd32bb1fdd8dc8674a
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-26 10:42:51 +0200

    .

--- act2/act4.js
+++ act2/act4.js
@@ -1,16 +1,7 @@
 var navegador = navigator.userAgent;
 if (navegador != "msie") {
     alert("OPERACION NO PERMITIDA EN ESTE NAVEGADOR");
-    /*
-    var miVentanaAltura = screen.height;    
-    var miVentanaAnchura = screen.width;
-
-    if (miVentanaAltura != 500, miVentanaAnchura != 500) {
-        miVentana.resizeTo(500, 500);
-    }
-    */
-
-    
+       
 }else{
     var nueva = window.open("","pagina2","toolbar=yes,location=no,menubar=yes"+ "width=500,height=500");
 
