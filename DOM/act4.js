7f1c4c7672b55c655509e45c501340607dd8da60
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-10-10 11:40:13 +0200

    hecho

--- DOM/act4.js
+++ DOM/act4.js
@@ -1,7 +1,18 @@
-function imagenes(){
+window.onload = function(){
+    var links = document.getElementById('links');
+    var imagenes = document.getElementById('imagen');
 
-}
+    links.addEventListener("click",contarLinks);
+    imagenes.addEventListener("click",contarImagenes);
 
-function links(){
     
+    function contarLinks(){
+        var contador = document.getElementsByTagName('a');
+        alert("Hay " + contador.length + " links");
+    }
+    function contarImagenes(){
+        var contador = document.getElementsByTagName('img');
+        alert("Hay " + contador.length + " imagenes");
+    }
 }
+
