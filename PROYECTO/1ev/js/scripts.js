e9b2f126c9b9e9e4eeea085051d310f5c1ba058b
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-10-25 11:47:58 +0200

    s

--- PROYECTO/1ev/js/scripts.js
+++ PROYECTO/1ev/js/scripts.js
@@ -5,14 +5,9 @@ $(window).load(function() {
     });
   });
 
+  //LINK EN TARJETAS
+
 function comprar() {
   window.location.href = "http://127.0.0.1:5500/html/tarifas.html";
 }
 
-const acordeon = document.getElementsByClassName('contenedor');
-
-for (i=0; i<acordeon.length; i++) {
-  acordeon[i].addEventListener('click', function () {
-    this.classList.toggle('activa')
-  })
-}
