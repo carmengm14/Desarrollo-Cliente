f35d0f1aaac1992c8e0fe5b6deb59718998fb34d
carmengm14 <alugar4049@ieselcaminas.org>
2022-11-09 20:33:22 +0100

    repasando :p

--- act2 avanzado/act5.js
+++ act2 avanzado/act5.js
@@ -1,3 +1,6 @@
-alert('BIENVENIDO A LA PAGINA');
-setTimeout(window.open("act4.html"),2000);
+if (confirm('BIENVENIDO A LA PÁGINA')) {
+    setTimeout(window.open("act4.html"),20000);
+}else{
+    document.write('NO SE TE HA REDIRIGIDO A LA PÁGINA AL CANCELAR')
+}
 
