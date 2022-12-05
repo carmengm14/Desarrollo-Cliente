746136ec92b1eaf4092c3209f02823371b5fc43a
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-12-05 11:29:19 +0100

    acabado

---
+++ AJAX/ejer8.js
@@ -0,0 +1,27 @@
+var XMLHttpRequestObject = false;
+if (window.XMLHttpRequest) {
+    XMLHttpRequestObject = new XMLHttpRequest();
+} else if (window.ActiveXObject) {
+    XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+function sacardatos(){
+    if(XMLHttpRequestObject) {
+        var lugar = document.getElementById("contenedor");
+        XMLHttpRequestObject.open("GET", 'ejer8.json');
+        XMLHttpRequestObject.onreadystatechange = function(){
+        if (XMLHttpRequestObject.readyState == 4 &&
+        XMLHttpRequestObject.status == 200) {
+        objeto = JSON.parse(XMLHttpRequestObject.responseText);
+        lugar.innerHTML = objeto.firstName + " " + objeto.lastName;
+    }
+
+    
+}
+    XMLHttpRequestObject.send(null);
+    }
+}
+
+window.onload = function () {
+    document.getElementById('boton').onclick = sacardatos;
+}
