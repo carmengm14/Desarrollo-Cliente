ebe6def8e0934d760e3521c35c908e49d280b854
carmengm14 <alugar4049@ieselcaminas.org>
2023-02-04 20:44:49 +0100

    CON APUNTE

--- RepasoExamen2ev/Ajax/ejer3/ejer.js
+++ RepasoExamen2ev/Ajax/ejer3/ejer.js
@@ -21,3 +21,32 @@ function cambiarXML(){
         XMLHttpRequestObject.send(null);
     }
 }
+
+/**
+ * SI HUBIESE MÁS DE UN DATO EN EL XML SE PONDRIA LA FUNCION ASI:
+ * function cargarXML() {
+    if(XMLHttpRequestObject){
+        XMLHttpRequestObject.open("GET", "./datos.xml")
+        XMLHttpRequestObject.onreadystatechange = function(){
+            if (XMLHttpRequestObject.readyState == 4 &&
+                XMLHttpRequestObject.status == 200) {
+                var contenedor = document.getElementById('contenedor')
+                var resultado= "";
+                var xml = XMLHttpRequestObject.responseXML;
+                var agendas = xml.getElementsByTagName('agenda')
+                for (let i = 0; i < agendas.length; i++) {
+                    var agenda = agendas[i]
+                    var nombre = agenda.getElementsByTagName('nombre')[0].firstChild.nodeValue
+                    var apellidos = agenda.getElementsByTagName('apellidos')[0].firstChild.nodeValue
+                    var telf = agenda.getElementsByTagName('telf')[0].firstChild.nodeValue
+                    var email = agenda.getElementsByTagName('email')[0].firstChild.nodeValue
+
+                    resultado = "<p>"+ nombre + " "+ apellidos + " "+ telf + " "+ email +"</p>"                    
+                }
+                contenedor.innerHTML = resultado;
+            }
+        }
+        XMLHttpRequestObject.send(null);
+    }
+}
+*/
