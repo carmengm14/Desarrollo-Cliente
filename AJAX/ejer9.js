a8201f933abc55dc7029c82642c81908045346be
carmengm14 <alugar4049@ieselcaminas.org>
2023-02-06 19:41:33 +0100

    resumido y hecho

--- AJAX/ejer9.js
+++ AJAX/ejer9.js
@@ -1,30 +1,30 @@
 var XMLHttpRequestObject = false;
-    if (window.XMLHttpRequest) {
+if (window.XMLHttpRequest) {
     XMLHttpRequestObject = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
+} else if (window.ActiveXObject) {
     XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP");
-}    
+}
 
-function sacardatos(){
-    if(XMLHttpRequestObject) {
+function sacardatos() {
+    if (XMLHttpRequestObject) {
         let imagen = document.createElement('img');
         imagen.setAttribute('src', 'gif.gif');
         imagen.setAttribute('id', 'imagen');
         var lugar = document.getElementById("contenedor");
         lugar.appendChild(imagen);
         XMLHttpRequestObject.open("GET", 'ejer9.php');
-        XMLHttpRequestObject.onreadystatechange = function(){
-        if (XMLHttpRequestObject.readyState == 4 &&
-        XMLHttpRequestObject.status == 200) {
-        lugar.innerHTML = XMLHttpRequestObject.responseText;        
-        imagen.remove();
-        }
+        XMLHttpRequestObject.onreadystatechange = function () {
+            if (XMLHttpRequestObject.readyState == 4 &&
+                XMLHttpRequestObject.status == 200) {
+                lugar.innerHTML = XMLHttpRequestObject.responseText;
+                imagen.remove();
+            }
 
-    }
-    XMLHttpRequestObject.send(null);
+        }
+        XMLHttpRequestObject.send(null);
     }
 }
 
-window.onload = function(){
+window.onload = function () {
     document.getElementById('boton').onclick = sacardatos
 }
