245ed0731f816d9356592d4cd962cab3c0f9058b
carmengm14 <carmen.garcia.monreal@gmail.com>
2023-02-14 11:46:34 +0100

    no funciona

--- TYPESCRIPT/ejer3.js
+++ TYPESCRIPT/ejer3.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    let valor1 = $("#numero1").val();
-    let valor2 = $("#numero2").val();
-    let resultado = valor1 + valor2;
+    var valor1 = $("#numero1").val();
+    var valor2 = $("#numero2").val();
+    var resultado = valor1 + valor2;
     $("body").append("<br><br><input type='text' name='resultado' id='resultado'> <br><br> <button id='boton'>Resultado</button>");
     $("#boton").click(function(){
         $("#resultado").val() = resultado;
