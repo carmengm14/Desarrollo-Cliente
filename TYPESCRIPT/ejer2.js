d7267a01cafde950d223dbf7b15bbda9c17b61fb
carmengm14 <carmen.garcia.monreal@gmail.com>
2023-02-14 10:39:09 +0100

    ejer2 hecho

---
+++ TYPESCRIPT/ejer2.js
@@ -0,0 +1,4 @@
+var precio;
+precio = window.prompt("Escribe un precio");
+var resultado = (precio * 1.21);
+window.alert(resultado.toFixed(2));
---
+++ TYPESCRIPT/ejer2.ts
@@ -0,0 +1,4 @@
+let precio : any ;
+precio = window.prompt("Escribe un precio");
+let resultado : number = (precio * 1.21);
+window.alert(resultado.toFixed(2));
