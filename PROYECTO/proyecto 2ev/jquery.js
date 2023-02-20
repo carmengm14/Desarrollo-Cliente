17fce48634da39fe283156bf1106746dee95936b
carmengm14 <carmen.garcia.monreal@gmail.com>
2023-02-20 09:38:38 +0100

    proyecto

---
+++ PROYECTO/proyecto 2ev/jquery.js
@@ -0,0 +1,34 @@
+/**LA CALCULADORA */
+$(function(){
+    $(".val").click(function(e){
+         e.preventDefault();
+          var a = $(this).attr("href");
+          $(".screen").append(a);
+          $(".outcome").val($(".outcome").val() + a);
+    });
+
+     $(".equal").click(function(){
+          $(".outcome").val(eval($(".outcome").val()));
+          $(".screen").html(eval($(".outcome").val()));
+     });
+
+     $(".clear").click(function(){
+          $(".outcome").val("");
+          $(".screen").html("");
+     });
+
+     $(".min").click(function(){
+         $(".cal").stop().animate({width: "0px", height: "0px", marginLeft: "700px", marginTop: "1000px"}, 500);
+        setTimeout(function(){$(".cal").css("display", "none")}, 600);
+     });
+
+     $(".close").click(function(){
+          $(".cal").css("display", "none");
+     });
+
+     $("#contenedor").html("<br><br> <center> ESTE TEXTO ESTA ESCRITO EN EL JS </center>");
+     $("#contenedor").css("color","green");
+})
+/**FIN DE LA CALCULADORA */
+
+/*TEXTO ESCRITO EN JS*/
